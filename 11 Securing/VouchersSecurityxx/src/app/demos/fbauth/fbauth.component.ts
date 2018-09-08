72aad8ba4636c403e14d1dc12f176945d5a6b8ca
Alexander Pajer <alexander.pajer@integrations.at>
2018-09-08 12:00:26 +0200

    security buildable

---
+++ 11 Securing/VouchersSecurityxx/src/app/demos/fbauth/fbauth.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-fbauth',
+  templateUrl: './fbauth.component.html',
+  styleUrls: ['./fbauth.component.css']
+})
+export class FacebookAuthComponent implements OnInit {
+
+  constructor(private httpClient : HttpClient) { }
+
+  ngOnInit() {
+  }
+
+  useFBAuth(){
+    this.httpClient.get("http://localhost:5000/api/authapi/usefbauth/").subscribe(data=>console.log(data));
+  }
+
+}
---
+++ 11 Securing/VouchersSecurityxx/src/app/demos/firebase/firebase.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-firebase',
+  templateUrl: './firebase.component.html',
+  styleUrls: ['./firebase.component.css']
+})
+export class FirebaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  logIn(){
+    
+  }
+
+  logOut(){
+
+  }
+
+
+}
