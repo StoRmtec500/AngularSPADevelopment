f6f4ad958c84d03ed95ab20db7af4100801f75d3
Administrator@SPDOM.LOCAL <Administrator@SPDOM.LOCAL>
2018-09-19 10:31:17 +0200

    spell err corrected

--- 05 UI/VouchersUI/src/app/demos/cssbinding/binding.component.ts
+++ 05 UI/VouchersUI/src/app/demos/cssbinding/binding.component.ts
@@ -1,40 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-binding',
-  templateUrl: './binding.component.html',
-  styleUrls: ['./binding.component.css']
+  selector: "app-binding",
+  templateUrl: "./binding.component.html",
+  styleUrls: ["./binding.component.css"]
 })
 export class CSSBindingComponent implements OnInit {
-
   addClass = true;
   addStyle = true;
   anotherProperty = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   setClasses() {
-    let classes =  {
-        extraclass: this.addClass,    
-        anotherclass: this.anotherProperty, 
+    let classes = {
+      extraClass: this.addClass,
+      anotherClass: this.anotherProperty
     };
     return classes;
   }
 
   setStyles() {
-    let styles = {        
-        'font-style':  this.addStyle ? 'italic' : 'normal',     
-        'font-weight': this.anotherProperty ? 'bold'   : 'normal',
+    let styles = {
+      "font-style": this.addStyle ? "italic" : "normal",
+      "font-weight": this.anotherProperty ? "bold" : "normal"
     };
     return styles;
   }
-
 }
 
 class Person {
   name: string;
   gender: string;
-  }
+}
