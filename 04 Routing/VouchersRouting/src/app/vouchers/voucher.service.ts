4cdc47839168d9ff557b6226d15d232a3789c77a
Hans der Schlächter <alexander.pajer@integrations.at>
2018-11-24 08:45:27 +0100

    routing

--- 04 Routing/VouchersRouting/src/app/vouchers/voucher.service.ts
+++ 04 Routing/VouchersRouting/src/app/vouchers/voucher.service.ts
@@ -1,33 +1,34 @@
-
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 import { Voucher } from "../shared/index";
-import { environment } from 'environments/environment';
+import { environment } from "environments/environment";
 
 @Injectable()
 export class VouchersService {
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-    delay : number = 0;
+  delay: number = 0;
 
-    vouchers = null;
+  vouchers = null;
 
-    getVouchers() : Promise<any> {
-        return this.http.get(environment.vouchersAPI).toPromise();          
-    }
-    
-    getVoucher(id: number) : Promise<any> {
-        return new Promise<Voucher>((resolve, reject)=>{
-            setTimeout(()=>{
-                this.http.get(environment.vouchersAPI).toPromise()
-            .then((data: Voucher[])=>{
-                var v = data.filter((item)=>{
-                    return item.ID == id;
-                 })
-                 resolve(v[0]);
-            })
-            .catch(err=>reject(err));
-            }, 1000 * this.delay)
-        })
-    }
+  getVouchers(): Promise<any> {
+    return this.http.get(environment.vouchersAPI).toPromise();
+  }
+
+  getVoucher(id: number): Promise<any> {
+    return new Promise<Voucher>((resolve, reject) => {
+      setTimeout(() => {
+        this.http
+          .get(environment.vouchersAPI)
+          .toPromise()
+          .then((data: Voucher[]) => {
+            var v = data.find(item => {
+              return item.ID == id;
+            });
+            resolve(v);
+          })
+          .catch(err => reject(err));
+      }, 1000 * this.delay);
+    });
+  }
 }
