5fc5d7223420e67c55a566a2781534ff0ca66d1a
unknown <Administrator@SPDOM.LOCAL>
2018-12-05 16:56:00 +0100

    resolver

--- 04 Routing/VouchersRouting/src/app/vouchers/voucher/voucher-resolver.service.ts
+++ 04 Routing/VouchersRouting/src/app/vouchers/voucher/voucher-resolver.service.ts
@@ -1,18 +1,18 @@
-import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Injectable } from "@angular/core";
+import { Resolve, ActivatedRouteSnapshot } from "@angular/router";
 
-import { VouchersService } from '../voucher.service';
-import { Voucher } from '../../shared';
-import { Observable } from 'rxjs';
+import { VouchersService } from "../voucher.service";
+import { Voucher } from "../../shared";
+import { Observable } from "rxjs";
 
 @Injectable()
 export class VoucherResolver implements Resolve<Voucher> {
+  constructor(private vs: VouchersService) {}
 
-  constructor(private vs: VouchersService) { }
-
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Voucher | Observable<Voucher> | Promise<Voucher> {
-    let id = +route.params['id'];
+  resolve(
+    route: ActivatedRouteSnapshot
+  ): Voucher | Observable<Voucher> | Promise<Voucher> {
+    let id = +route.params["id"];
     return this.vs.getVoucher(id);
-  } 
-
+  }
 }
