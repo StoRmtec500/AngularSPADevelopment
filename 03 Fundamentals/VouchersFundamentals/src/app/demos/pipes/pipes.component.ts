81547eef1756bf7250fd284f9e5a7f2ebe5dad63
unknown <Administrator@SPDOM.LOCAL>
2018-12-05 17:23:49 +0100

    fundamentals fix

--- 03 Fundamentals/VouchersFundamentals/src/app/demos/pipes/pipes.component.ts
+++ 03 Fundamentals/VouchersFundamentals/src/app/demos/pipes/pipes.component.ts
@@ -1,22 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-pipes',
-  templateUrl: './pipes.component.html',
-  styleUrls: ['./pipes.component.css']
+  selector: "app-pipes",
+  templateUrl: "./pipes.component.html",
+  styleUrls: ["./pipes.component.css"]
 })
 export class PipesComponent implements OnInit {
-
-  person = {id: 1, name: "Alex", age: 47, imgUrl: "", salery: 2000, dateOfBirth: new Date(1970, 3, 2,15, 0) }; // 2.4.70
+  person = {
+    id: 1,
+    name: "Soi",
+    age: 6,
+    imgUrl: "",
+    salery: 2000,
+    dateOfBirth: new Date(2010, 13, 2, 15, 0)
+  };
 
   persons = [
-    {name: "Heinz", gender: "male"}, 
-    {name: "Brunhilde", gender: "female"},
-    {name: "Susi", gender: "female"}];
+    { name: "Heinz", gender: "male" },
+    { name: "Brunhilde", gender: "female" },
+    { name: "Susi", gender: "female" }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
