76d3f1d2b063d8d3176d6c015e057b25f98ecf88
Alexander Pajer <alexander.pajer@integrations.at>
2019-01-18 15:08:26 +0100

    mat table click

--- 05 UI/VouchersUI/src/app/demos/material-table/material-table.component.ts
+++ 05 UI/VouchersUI/src/app/demos/material-table/material-table.component.ts
@@ -1,30 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material';
-import { Voucher } from '../../shared/index';
-import { HttpClient } from '@angular/common/http';
-import { Http } from '@angular/http';
+import { Component, OnInit } from "@angular/core";
+import { MatTableDataSource } from "@angular/material";
+import { Voucher } from "../../shared/index";
+import { HttpClient } from "@angular/common/http";
+import { Http } from "@angular/http";
 
 @Component({
-  selector: 'app-material-table',
-  templateUrl: './material-table.component.html',
-  styleUrls: ['./material-table.component.scss']
+  selector: "app-material-table",
+  templateUrl: "./material-table.component.html",
+  styleUrls: ["./material-table.component.scss"]
 })
 export class MaterialTableComponent implements OnInit {
-
   vouchers: Voucher[];
   dataSource: MatTableDataSource<Voucher>;
-  displayedColumns = ['ID', 'Text', 'Date', 'Amount'];
+  displayedColumns = ["ID", "Text", "Date", "Amount"];
 
-  constructor(private http:Http) {  }
+  constructor(private http: Http) {}
 
   ngOnInit() {
-    this.http.get('./assets/vouchers.json').toPromise().then(data => {
-      this.vouchers = data.json();
-      console.log(this.vouchers)
-      this.dataSource = new MatTableDataSource(data.json());
-    })    
-  }  
-
+    this.http
+      .get("./assets/vouchers.json")
+      .toPromise()
+      .then(data => {
+        this.vouchers = data.json();
+        console.log(this.vouchers);
+        this.dataSource = new MatTableDataSource(data.json());
+      });
+  }
 
   applyFilter(filterValue: string) {
     filterValue = filterValue.trim(); // Remove whitespace
@@ -32,4 +33,7 @@ export class MaterialTableComponent implements OnInit {
     this.dataSource.filter = filterValue;
   }
 
+  editItem(row) {
+    console.log("Edit Row", row);
+  }
 }
