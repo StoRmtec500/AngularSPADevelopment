e10425312083fc8cb6f8836ca69416b97ecd4198
Alexander Pajer <alexander.pajer@integrations.at>
2019-01-19 08:37:06 +0100

    seperate sec sample delete

--- 11 Securing/AdalGraph/src/app/endpoints.ts
+++
@@ -1,4 +0,0 @@
-export const eps = {
-  graphApiUri: "https://graph.microsoft.com",
-  sharePointUri: "https://integrationsonline.sharepoint.com" // Replace with your Tenant & Make sure you assign permissions in Azure AD
-};
