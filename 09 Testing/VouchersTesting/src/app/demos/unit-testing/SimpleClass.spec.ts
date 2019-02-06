fe48a51d983d1a1366b7d5f4e96ba39cbebf1192
Alexander Pajer <alexander.pajer@integrations.at>
2019-02-06 09:23:29 +0100

    wallaby config updated

--- 09 Testing/VouchersTesting/src/app/demos/unit-testing/SimpleClass.spec.ts
+++ 09 Testing/VouchersTesting/src/app/demos/unit-testing/SimpleClass.spec.ts
@@ -4,7 +4,7 @@ describe("Hello world", function() {
   it("contains 12 charactes", function() {
     expect(SimpleClass.sayHelloWorld().length).toEqual(12);
   });
-  it("says hello world", function() {
-    expect(SimpleClass.sayHelloWorld()).toEqual("Hello world!");
+  it("says 'Hello World!", function() {
+    expect(SimpleClass.sayHelloWorld()).toEqual("Hello World!");
   });
 });
