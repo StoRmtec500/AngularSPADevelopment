756f112d836c103abb616d9fde2fdd5568da9f5e
Alexander Pajer <alexander.pajer@integrations.at>
2019-02-08 09:31:00 +0100

    Simple Comp Test

--- 09 Testing/VouchersTesting/src/app/demos/simple-comp/food.component.spec.ts
+++ 09 Testing/VouchersTesting/src/app/demos/simple-comp/food.component.spec.ts
@@ -2,7 +2,7 @@ import { FoodComponent } from "./food.component";
 import { FoodItem } from "./food-items";
 import { of } from "rxjs";
 
-describe("FoodComponent - using Food Service", () => {
+describe("FoodComponent:", () => {
   let comp: FoodComponent;
   let foodData: FoodItem[];
   let mockFS;
@@ -13,25 +13,29 @@ describe("FoodComponent - using Food Service", () => {
     { name: "Cordon Bleu", rating: 2 }
   ];
 
-  let serviceResult = foodData.splice(3, 1);
+  let serviceResult = [
+    { name: "Pad Thai", rating: 5 },
+    { name: "Butter Chicken", rating: 5 },
+    { name: "Cannelloni", rating: 4 }
+  ];
+  debugger;
 
   beforeEach(() => {
     mockFS = jasmine.createSpyObj(["getItems", "deleteItem"]);
     comp = new FoodComponent(mockFS);
   });
 
-  it("removes the item from the list"),
-    () => {
-      comp.food = foodData;
-      mockFS.deleteItem.and.returnValue(of(foodData.splice(3, 1)));
-      comp.deleteFood(foodData[3]);
+  it("removes the item from the list", () => {
+    comp.food = foodData;
+    mockFS.deleteItem.and.returnValue(of(serviceResult));
+    comp.deleteFood(foodData[3]);
 
-      expect(comp.food.length).toBe(3);
-    };
+    expect(comp.food.length).toBe(3);
+  });
 
   it("should call deleteHero", () => {
     comp.food = foodData;
-    // mockFS.deleteFood.and.returnValue(of(foodData.splice(3,1))
+    mockFS.deleteItem.and.returnValue(of(serviceResult));
 
     comp.deleteFood(foodData[3]);
     expect(mockFS.deleteItem).toHaveBeenCalledWith(foodData[3]);
