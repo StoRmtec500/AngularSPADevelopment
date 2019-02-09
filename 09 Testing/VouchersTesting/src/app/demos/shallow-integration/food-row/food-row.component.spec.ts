cc37342b99f505cefc0fd06407c733243c42a952
Alexander Pajer <alexander.pajer@integrations.at>
2019-02-09 15:45:10 +0100

    shallow finished

--- 09 Testing/VouchersTesting/src/app/demos/shallow-integration/food-row/food-row.component.spec.ts
+++ 09 Testing/VouchersTesting/src/app/demos/shallow-integration/food-row/food-row.component.spec.ts
@@ -2,6 +2,7 @@ import { FoodRowComponent } from "./food-row.component";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { NO_ERRORS_SCHEMA } from "@angular/core";
 import { RatingPipe } from "../../pipe/rating.pipe";
+import { By } from "@angular/platform-browser";
 
 describe("Food Row Integration Test", () => {
   let testModule = {
@@ -19,5 +20,17 @@ describe("Food Row Integration Test", () => {
   it("should have the correct food item", () => {
     fixture.componentInstance.food = { name: "Pad Thai", rating: 5 };
     expect(fixture.componentInstance.food.name).toEqual("Pad Thai");
+  });
+
+  it("should render name bold", () => {
+    fixture.componentInstance.food = { name: "Pad Thai", rating: 5 };
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector("span").textContent).toContain(
+      "Pad Thai"
+    );
+
+    expect(
+      fixture.debugElement.query(By.css("#bogus")).nativeElement.textContent
+    ).toContain("Pad Thai");
   });
 });
