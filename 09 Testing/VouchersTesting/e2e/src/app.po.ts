97c0936c092203003a92dc0799d37671e626beec
Alexander Pajer <alexander.pajer@integrations.at>
2019-02-19 20:09:43 +0100

    e2e testupdate

--- 09 Testing/VouchersTesting/e2e/src/app.po.ts
+++ 09 Testing/VouchersTesting/e2e/src/app.po.ts
@@ -1,11 +1,11 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get('/');
-  }
+	navigateTo() {
+		return browser.get('/demos/introe2e');
+	}
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
-  }
+	getParagraphText() {
+		return element(by.css('mat-sidenav > div > mat-toolbar > h4')).getText();
+	}
 }
