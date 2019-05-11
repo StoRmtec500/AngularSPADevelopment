efe01fd607e9d115697bcc99575520e374f030b1
Alexander Pajer <alexander.pajer@integrations.at>
2019-05-11 09:07:27 +0200

    service update

--- 12 Publishing/VouchersAPI/VouchersAPI/Startup.cs
+++ 12 Publishing/VouchersAPI/VouchersAPI/Startup.cs
@@ -17,53 +17,47 @@ using Newtonsoft.Json.Serialization;
 using NLog.Extensions.Logging;
 using NLog.Web;
 
-namespace Vouchers
-{
-    public class Startup
-    {
+namespace Vouchers {
+    public class Startup {
         private readonly IHostingEnvironment env;
 
-        public Startup(IHostingEnvironment environment)
-        {
+        public Startup (IHostingEnvironment environment) {
             env = environment;
         }
 
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices (IServiceCollection services) {
             //Config
-            var cfgBuilder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json");
-            var configuration = cfgBuilder.Build();
-            services.Configure<VouchersConfig>(configuration);
-            services.AddSingleton(typeof(IConfigurationRoot), configuration);
-            var conStr = configuration["ConnectionStrings:DockerConnection"];
+            var cfgBuilder = new ConfigurationBuilder ()
+                .SetBasePath (env.ContentRootPath)
+                .AddJsonFile ("appsettings.json");
+            var configuration = cfgBuilder.Build ();
+            services.Configure<VouchersConfig> (configuration);
+            services.AddSingleton (typeof (IConfigurationRoot), configuration);
+            var conStr = configuration["ConnectionStrings:SQLServerDBConnection"];
 
             //EF
-            services.AddEntityFrameworkSqlServer()
-                .AddDbContext<VouchersDBContext>(options => options.UseSqlServer(conStr));
-            services.AddScoped<IVouchersRepository, VouchersRepository>();
+            services.AddEntityFrameworkSqlServer ()
+                .AddDbContext<VouchersDBContext> (options => options.UseSqlServer (conStr));
+            services.AddScoped<IVouchersRepository, VouchersRepository> ();
 
             //Simple Windows Auth
-            services.AddAuthentication(HttpSysDefaults.AuthenticationScheme);
+            services.AddAuthentication (HttpSysDefaults.AuthenticationScheme);
 
             //Firebase
             var fbrealm = configuration["Authentication:Firebase:Realm"];
 
             services
-            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
-            {
-                options.Authority = "https://securetoken.google.com/" + fbrealm;
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidIssuer = "https://securetoken.google.com/" + fbrealm,
-                    ValidateAudience = true,
-                    ValidAudience = fbrealm,
-                    ValidateLifetime = true
-                };
-            });
+                .AddAuthentication (JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer (options => {
+                    options.Authority = "https://securetoken.google.com/" + fbrealm;
+                    options.TokenValidationParameters = new TokenValidationParameters {
+                        ValidateIssuer = true,
+                        ValidIssuer = "https://securetoken.google.com/" + fbrealm,
+                        ValidateAudience = true,
+                        ValidAudience = fbrealm,
+                        ValidateLifetime = true
+                    };
+                });
 
             //Identity 
             //
@@ -91,77 +85,66 @@ namespace Vouchers
             // For specific URL ie. your Angular CLI Frontend use: 
             // corsBuilder.WithOrigins("http://localhost:4200")           
 
-            services.AddCors(options =>
-            {
-                options.AddPolicy("AllowAll",
-                    builder => builder.AllowAnyOrigin()
-                        .AllowAnyMethod()
-                        .AllowAnyHeader()
-                        .AllowAnyOrigin()
-                        .AllowCredentials());
+            services.AddCors (options => {
+                options.AddPolicy ("AllowAll",
+                    builder => builder.AllowAnyOrigin ()
+                    .AllowAnyMethod ()
+                    .AllowAnyHeader ()
+                    .AllowAnyOrigin ()
+                    .AllowCredentials ());
             });
 
             //Serialization Options
-            services.AddMvc().AddJsonOptions(ser =>
-            {
+            services.AddMvc ().AddJsonOptions (ser => {
                 ser.SerializerSettings.ContractResolver =
-                    new DefaultContractResolver();
+                    new DefaultContractResolver ();
             });
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory,
-            VouchersDBContext dbcontext)
-        {
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory,
+            VouchersDBContext dbcontext) {
             //Logging
             // loggerFactory.AddNLog();
             // env.ConfigureNLog("nlog.config");
 
-            var jsnlogConfiguration = new JsnlogConfiguration();
-            app.UseJSNLog(new LoggingAdapter(loggerFactory), jsnlogConfiguration);
+            var jsnlogConfiguration = new JsnlogConfiguration ();
+            app.UseJSNLog (new LoggingAdapter (loggerFactory), jsnlogConfiguration);
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseStatusCodePages();
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+                app.UseStatusCodePages ();
             }
 
             //Startup File for serving a *.html as default
 
-            DefaultFilesOptions options = new DefaultFilesOptions();
-            options.DefaultFileNames.Clear();
-            options.DefaultFileNames.Add("index.html");
-            app.UseDefaultFiles(options);
+            DefaultFilesOptions options = new DefaultFilesOptions ();
+            options.DefaultFileNames.Clear ();
+            options.DefaultFileNames.Add ("index.html");
+            app.UseDefaultFiles (options);
 
-            if (env.IsDevelopment())
-            {
-                app.UseStaticFiles(new StaticFileOptions
-                {
-                    OnPrepareResponse = context =>
-                    {
+            if (env.IsDevelopment ()) {
+                app.UseStaticFiles (new StaticFileOptions {
+                    OnPrepareResponse = context => {
                         context.Context.Response.Headers["Cache-Control"] = "no-cache, no-store";
                         context.Context.Response.Headers["Pragma"] = "no-cache";
                         context.Context.Response.Headers["Expires"] = "-1";
                     }
                 });
-            }
-            else
-            {
-                app.UseStaticFiles();
+            } else {
+                app.UseStaticFiles ();
             }
 
             //Cors
-            app.UseCors("AllowAll");
+            app.UseCors ("AllowAll");
 
             //Auth
-            app.UseAuthentication();
+            app.UseAuthentication ();
 
-            app.UseMvc();
+            app.UseMvc ();
         }
 
-        private static void SetPasswordOptions(IServiceCollection services)
-        {
-            services.Configure<IdentityOptions>(options =>
-            {
+        private static void SetPasswordOptions (IServiceCollection services) {
+            services.Configure<IdentityOptions> (options => {
                 // Password settings
                 options.Password.RequireDigit = false;
                 options.Password.RequiredLength = 0;
@@ -171,7 +154,7 @@ namespace Vouchers
                 options.Password.RequiredUniqueChars = 1;
 
                 // Lockout settings
-                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(1);
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes (1);
                 options.Lockout.MaxFailedAccessAttempts = 10;
                 options.Lockout.AllowedForNewUsers = true;
 
