335e3911c1227ff8d80b921ac226db0f9b297b7b
Alexander Pajer <alexander.pajer@integrations.at>
2019-06-04 14:50:06 +0200

    minor fix

--- 03 Fundamentals/VouchersFundamentals/src/app/demos/persons/person-edit/person-edit.component.ts
+++ 03 Fundamentals/VouchersFundamentals/src/app/demos/persons/person-edit/person-edit.component.ts
@@ -1,29 +1,27 @@
-import { Component, Input, OnInit, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
-import { Person } from '../person';
+import { Component, Input, OnInit, Output, EventEmitter } from "@angular/core";
+import { Person } from "../person";
 
 @Component({
-	selector: 'app-person-edit',
-	templateUrl: './person-edit.component.html',
-	styleUrls: [ './person-edit.component.css' ],
-	changeDetection: ChangeDetectionStrategy.OnPush
+  selector: "app-person-edit",
+  templateUrl: "./person-edit.component.html",
+  styleUrls: ["./person-edit.component.css"]
 })
 export class PersonEditComponent implements OnInit {
-	@Input() person: Person;
-	@Input() editMode: boolean;
+  @Input() person: Person;
+  @Input() editMode: boolean;
+  @Output() savePerson: EventEmitter<Person> = new EventEmitter();
 
-	@Output() savePerson: EventEmitter<Person> = new EventEmitter();
+  constructor() {}
 
-	constructor() {}
+  ngOnInit() {
+    console.log(this.person);
+  }
 
-	ngOnInit() {
-		console.log(this.person);
-	}
+  doSave() {
+    this.savePerson.emit(this.person);
+  }
 
-	doSave() {
-		this.savePerson.emit(this.person);
-	}
-
-	doDelete() {
-		console.log(`deleting ${this.person.name}`);
-	}
+  doDelete() {
+    console.log(`deleting ${this.person.name}`);
+  }
 }
